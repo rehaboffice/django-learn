4a8335f6ba0feed87688cdd89987e3f10f91fa2b
rehaboffice <rehabtoffice@gmail.com>
2025-06-16 14:43:46 +0500

    `Finalised personal website`

--- personal_website/pages/views.py
+++ personal_website/pages/views.py
@@ -4,3 +4,8 @@ from django.http import HttpResponse
 # Create your views here.
 def home_page_view(request):
     return HttpResponse("Homepage")
+
+def about_page_view(request):
+    # return render(request, "pages/about.html")
+    context = {"name": "John Doe", "age": 30}
+    return render(request, "pages/about.html", context)
