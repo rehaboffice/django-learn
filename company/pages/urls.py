694e2317c1b81caa9f9b3e1e809b605ec10983f6
rehaboffice <rehabtoffice@gmail.com>
2025-06-17 10:07:06 +0500

    `TemplateView`

--- company/pages/urls.py
+++ company/pages/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import home_page_view
+from .views import home_page_view, AboutPageView
 
 urlpatterns = [
+    path("about/", AboutPageView.as_view()),
     path("", home_page_view),
 ]
--- company/pages/views.py
+++ company/pages/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
 # Create your views here.
 def home_page_view(request):
@@ -12,3 +13,12 @@ def home_page_view(request):
         "description": "This is a simple Django application.",
     }
     return render(request, "home.html", context)
+
+class AboutPageView(TemplateView):
+    template_name = "about.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["about_info"] = "This is the about page of our Django application."
+        context["team_members"] = ["Alice", "Bob", "Charlie"]
+        return context
